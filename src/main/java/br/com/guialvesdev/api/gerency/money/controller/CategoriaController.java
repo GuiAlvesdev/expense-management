26c57e49ea563b48158ae32064cc9ed2a56bdf04
GuiAlvesdev <guialves1353@gmail.com>
2022-11-19 23:30:58 -0300

    refactor controllers :recycle:

--- src/main/java/br/com/guialvesdev/api/gerency/money/controller/CategoriaController.java
+++ src/main/java/br/com/guialvesdev/api/gerency/money/controller/CategoriaController.java
@@ -1,10 +1,14 @@
 package br.com.guialvesdev.api.gerency.money.controller;
 
 
+import br.com.guialvesdev.api.gerency.money.event.RecursoCriadoEvent;
 import br.com.guialvesdev.api.gerency.money.model.Categoria;
 import br.com.guialvesdev.api.gerency.money.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -12,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -20,31 +25,34 @@ public class CategoriaController {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
 
 
     @GetMapping
-    public List<Categoria> listar(){
+    @PreAuthorize("hasAuthority"('ROLE_PESQUISAR_CATEGORIA') and hasAuthority('SCOPE_read')")public List<Categoria> listar(){
         return categoriaRepository.findAll();
 
     }
 
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and hasAuthority('SCOPE_write')")
     public ResponseEntity<Categoria> criar (@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 
     }
 
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and hasAuthority('SCOPE_read')")
     public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable  Long codigo){
-        Categoria categoria = categoriaRepository.getReferenceById(codigo);
-        return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+        Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+        return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
     }
 
 
