b11179c0d9c318a9e45f4690157dbd8fc577ee9a
GuiAlvesdev <guialves1353@gmail.com>
2022-11-22 20:12:43 -0300

    refactor all code :recycle:

---
+++ money-management/src/test/java/com/guialvesdev/money/management/MoneyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guialvesdev.money.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
