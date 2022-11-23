8878f497a303f09966ba05ad6f76ea225ee6d08d
GuiAlvesdev <guialves1353@gmail.com>
2022-11-23 14:45:04 -0300

    refactor all code :recycle:

--- cost-management/src/main/java/com/guialvesdev/cost/management/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guialvesdev.cost.management.repository;
-
-import com.guialvesdev.cost.management.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
