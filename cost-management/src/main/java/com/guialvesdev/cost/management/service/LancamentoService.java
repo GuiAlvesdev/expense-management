920a9de6144ddffbb0a2d3655aa097159ffd7bb6
GuiAlvesdev <guialves1353@gmail.com>
2022-11-23 12:26:18 -0300

    add tokens :construction:

---
+++ cost-management/src/main/java/com/guialvesdev/cost/management/service/LancamentoService.java
@@ -0,0 +1,33 @@
+package com.guialvesdev.cost.management.service;
+
+import com.guialvesdev.cost.management.model.Lancamento;
+import com.guialvesdev.cost.management.model.Pessoa;
+import com.guialvesdev.cost.management.repository.LancamentoRepository;
+import com.guialvesdev.cost.management.repository.PessoaRepository;
+import com.guialvesdev.cost.management.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LancamentoService {
+
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    public Lancamento salvar(Lancamento lancamento){
+        Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+        if (pessoa.isEmpty() || pessoa.get().isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+
+        return lancamentoRepository.save(lancamento);
+    }
+
+
+}
