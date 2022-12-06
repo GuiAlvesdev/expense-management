986ad37bb1dba2cac1abeb5aa65e43464d1a58bc
GuiAlvesdev <guialves1353@gmail.com>
2022-12-06 11:12:20 -0300

    att for prod :rocket:

--- src/main/java/com/guialvesdev/demo/config/AuthorizationServerConfig.java
+++ src/main/java/com/guialvesdev/demo/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.guialvesdev.demo.config;
 
 
+import com.guialvesdev.demo.token.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -49,11 +54,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
         endpoints
                 .authenticationManager(authenticationManager)
-                .accessTokenConverter(accessTokenConverter())
-                .tokenStore(tokenStore())
                 .userDetailsService(userDetailsService)
+                .tokenEnhancer(tokenEnhancerChain)
+                .tokenStore(tokenStore())
                 .reuseRefreshTokens(false);
     }
 
@@ -69,6 +78,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public TokenStore tokenStore() {
         return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
 
 
--- src/main/java/com/guialvesdev/demo/security/AppUserDetailsService.java
+++ src/main/java/com/guialvesdev/demo/security/AppUserDetailsService.java
@@ -27,7 +27,7 @@ public class AppUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
         Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-        return new User(email, usuario.getSenha(), getPermissoes(usuario));
+        return new UsuarioSistema(usuario, getPermissoes(usuario));
     }
 
     private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
@@ -35,4 +35,7 @@ public class AppUserDetailsService implements UserDetailsService {
         usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
         return authorities;
     }
+
+
+
 }
