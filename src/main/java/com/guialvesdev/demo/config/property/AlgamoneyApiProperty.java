d1777348da182f60c167d34659e78d75fb9a6111
GuiAlvesdev <guialves1353@gmail.com>
2022-12-19 11:35:10 -0300

    add origin permitida :rocket:

--- src/main/java/com/guialvesdev/demo/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/guialvesdev/demo/config/property/AlgamoneyApiProperty.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AlgamoneyApiProperty {
 
-    private String originPermitida = "https://extraordinary-tapioca-931ec3.netlify.app";
+    private String originPermitida = "*";
 
     private final Seguranca seguranca = new Seguranca();
 
