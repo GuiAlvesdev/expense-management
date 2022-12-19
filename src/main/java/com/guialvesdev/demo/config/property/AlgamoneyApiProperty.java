93f326866b97378677cc79b9ec3d7a52e9a38c5a
GuiAlvesdev <guialves1353@gmail.com>
2022-12-19 09:42:57 -0300

    add origin permitida :rocket:

--- src/main/java/com/guialvesdev/demo/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/guialvesdev/demo/config/property/AlgamoneyApiProperty.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AlgamoneyApiProperty {
 
-    private String originPermitida = "http://localhost:4200";
+    private String originPermitida = "https://guialvesdev.github.io";
 
     private final Seguranca seguranca = new Seguranca();
 
